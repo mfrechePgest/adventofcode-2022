046d38cfa1fd017d8612fa0d86c44d961a7ea633
Mikael FRECHE <mikael.freche@equasens.com>
2022-12-01 10:29:37 +0100

    Je l'ai pas fait en TDD mais bon j'ajoute un test pour essayer de me motiver à le faire les jours suivants

--- day01/src/main/java/Day1.java
+++ day01/src/main/java/Day1.java
@@ -9,32 +9,61 @@ import java.util.Map;
 public class Day1 {
 
     public static void main(String[] args) throws IOException {
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(Day1.class.getResourceAsStream("first_day.txt")))) {
+        Map<Integer, Long> caloriesPerElf = readFile("first_day.txt");
+        List<Map.Entry<Integer, Long>> topTreeElves = findTop3Elves(caloriesPerElf);
+
+        System.out.println("Step 1 : most Calories Elf is = " + ConsoleColors.cyan(topTreeElves.get(0).getKey()) +
+                " ; calories Count = " + ConsoleColors.cyan(step1Result(topTreeElves)));
+        System.out.println("Step 2 : Top Three elves are :");
+        for (Map.Entry<Integer, Long> e : topTreeElves) {
+            System.out.println("    " + ConsoleColors.cyan(e.toString()));
+        }
+        System.out.println("TOTAL : " + ConsoleColors.cyan(step2Result(topTreeElves)));
+    }
+
+    public Long step1(String fileName) throws IOException {
+        Map<Integer, Long> caloriesPerElf = readFile(fileName);
+        List<Map.Entry<Integer, Long>> top3 = findTop3Elves(caloriesPerElf);
+        return step1Result(top3);
+    }
+
+    public Long step2(String fileName) throws IOException {
+        Map<Integer, Long> caloriesPerElf = readFile(fileName);
+        List<Map.Entry<Integer, Long>> top3 = findTop3Elves(caloriesPerElf);
+        return step2Result(top3);
+    }
+
+    private static Long step1Result(List<Map.Entry<Integer, Long>> topTreeElves) {
+        return topTreeElves.get(0).getValue();
+    }
+
+    private static long step2Result(List<Map.Entry<Integer, Long>> topTreeElves) {
+        return topTreeElves.stream().mapToLong(Map.Entry::getValue).sum();
+    }
+
+    private static List<Map.Entry<Integer, Long>> findTop3Elves(Map<Integer, Long> caloriesPerElf) {
+        Comparator<Map.Entry<Integer, Long>> comparator = Comparator.comparingLong(Map.Entry::getValue);
+        List<Map.Entry<Integer, Long>> topTreeElves = caloriesPerElf.entrySet().stream().sorted(comparator.reversed()).limit(3).toList();
+        return topTreeElves;
+    }
+
+    private static Map<Integer, Long> readFile(String fileName) throws IOException {
+        Map<Integer, Long> caloriesPerElf = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(Day1.class.getResourceAsStream(fileName)))) {
             String line = br.readLine();
-            int currentElf = 1;
-            Map<Integer, Long> caloriesPerElf = new HashMap<>();
+            int currentElf = 1;            
 
             while (line != null) {
                 if (line.isBlank()) {
                     currentElf++;
                 } else {
                     Long cal = Long.valueOf(line);
-                    caloriesPerElf.put(currentElf, caloriesPerElf.getOrDefault(currentElf,0L) + cal);
+                    caloriesPerElf.put(currentElf, caloriesPerElf.getOrDefault(currentElf, 0L) + cal);
                 }
                 line = br.readLine();
             }
-
-            Comparator<Map.Entry<Integer,Long>> comparator = Comparator.comparingLong(Map.Entry::getValue);
-            List<Map.Entry<Integer, Long>> topTreeElves = caloriesPerElf.entrySet().stream().sorted(comparator.reversed()).limit(3).toList();
-
-            System.out.println("Step 1 : most Calories Elf is = " + ConsoleColors.cyan(topTreeElves.get(0).getKey()) + 
-                    " ; calories Count = " + ConsoleColors.cyan(topTreeElves.get(0).getValue()));
-            System.out.println("Step 2 : Top Three elves are :");
-            for ( Map.Entry<Integer,Long> e : topTreeElves ) {
-                System.out.println("    " + ConsoleColors.cyan(e.toString()));
-            }
-            System.out.println("TOTAL : " + ConsoleColors.cyan(topTreeElves.stream().mapToLong(e -> e.getValue()).sum()));
         }
+        return caloriesPerElf;
     }
 
 }
---
+++ day01/src/test/java/Day1Test.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+public class Day1Test {
+
+    public static final String SAMPLE_FILE = "sample.txt";
+
+    @Test
+    public void step1Test() throws IOException {
+        Day1 day1 = new Day1();
+        Long result = day1.step1(SAMPLE_FILE);
+        Assertions.assertEquals(24000, result);
+    }
+
+
+    @Test
+    public void step2Test() throws IOException {
+        Day1 day1 = new Day1();
+        Long result = day1.step2(SAMPLE_FILE);
+        Assertions.assertEquals(45000, result);
+    }
+
+}
