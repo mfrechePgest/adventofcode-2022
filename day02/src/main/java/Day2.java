40734bdcc4a446cf8ff16e8bee92a6fa21b56f06
Mikael FRECHE <mikael.freche@equasens.com>
2022-12-02 07:33:32 +0100

    Day 2

--- day02/src/main/java/Day2.java
+++ day02/src/main/java/Day2.java
@@ -1,8 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Day2 extends AbstractDay<Long, Long> {
 
+    public static final int WIN = 6;
+    private static final int DRAW = 3;
+    private static final int LOSS = 0;
+
+    private static final int ROCK = 1;
+    private static final int PAPER = 2;
+    private static final int SCISSORS = 3;
+
+    long score = 0;
+    long step2Score = 0;
 
     public Day2(String fileName) {
         super(fileName);
@@ -19,11 +30,11 @@ public class Day2 extends AbstractDay<Long, Long> {
     
 
     public Long resultStep1() {
-        return 0L;
+        return score;
     }
 
     public Long resultStep2() {
-        return 0L;
+        return step2Score;
     }
     
 
@@ -32,11 +43,48 @@ public class Day2 extends AbstractDay<Long, Long> {
             String line = br.readLine();
 
             while (line != null) {
-                // TODO
+                String[] chars = line.split(" ");
+                Play opp = Play.fromChar(chars[0]);
+                Play myPlay = Play.fromChar(chars[1]);
+                score += myPlay.score();
+                score += myPlay.fight(opp);
+                int expectedIssue = switch (chars[1]) {
+                    case "X" -> LOSS;
+                    case "Y" -> DRAW;
+                    case "Z" -> WIN;
+                    default -> throw new RuntimeException("Wat ?");
+                };
+                step2Score += expectedIssue;
+                step2Score += Arrays.stream(Play.values())
+                        .filter(p -> p.fight(opp) == expectedIssue)
+                        .mapToInt(Play::score)
+                        .findAny()
+                        .orElseThrow(() -> new RuntimeException("Oups"));
                 line = br.readLine();
             }
         }
     }
 
+    enum Play {
+        ROCK, PAPER, SCISSORS;
+
+        public int fight(Play p2) {
+            if (p2 == this) { return DRAW; };
+            return ((p2.ordinal() + 1 == this.ordinal()) || (this == ROCK && p2 == SCISSORS)) ? WIN : LOSS;
+        }
+
+        public int score() {
+            return this.ordinal() + 1;
+        }
+
+        public static Play fromChar(String s) {
+            return switch(s) {
+                case "A", "X" -> ROCK;
+                case "B", "Y" -> PAPER;
+                case "C", "Z" -> SCISSORS;
+                default -> null;
+            };
+        }
+    }
 
 }
--- day02/src/test/java/Day2Test.java
+++ day02/src/test/java/Day2Test.java
@@ -1,9 +1,17 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class Day2Test extends AbstractDayTest<Day2, Long, Long> {
 
     public static final String SAMPLE_FILE = "sample.txt";
 
     public Day2Test() {
-        super(() -> new Day2(SAMPLE_FILE), 0L, 0L);
+        super(() -> new Day2(SAMPLE_FILE), 15L, 12L);
     }
 
+    @Test
+    public void scissors_vs_paper() {
+        int result = Day2.Play.SCISSORS.fight(Day2.Play.PAPER);
+        Assertions.assertEquals(Day2.WIN, result);
+    }
 }
