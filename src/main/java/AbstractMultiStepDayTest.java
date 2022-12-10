0d3ce73f6d89b53de0181dd2ab74db0871c5e56b
Mikael FRECHE <mikael.freche@equasens.com>
2022-12-10 08:58:42 +0100

    Modif classe de test pour faciliter le debug d'un seul step

--- src/main/java/AbstractMultiStepDayTest.java
+++ src/main/java/AbstractMultiStepDayTest.java
@@ -1,14 +1,19 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import java.io.IOException;
 import java.util.function.Supplier;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public abstract class AbstractMultiStepDayTest<DAY extends AbstractMultiStepDay<STEP1, STEP2>, STEP1, STEP2> {
 
     private final Supplier<DAY> daySupplier;
     private final STEP1 expected1;
     private final STEP2 expected2;
+
+    private DAY day;
 
     public AbstractMultiStepDayTest(Supplier<DAY> daySupplier, STEP1 expected1, STEP2 expected2) {
         this.daySupplier = daySupplier;
@@ -16,19 +21,25 @@ public abstract class AbstractMultiStepDayTest<DAY extends AbstractMultiStepDay<
         this.expected2 = expected2;
     }
 
+    @BeforeAll
+    public void init() throws IOException {
+        day = daySupplier.get();
+        day.readFile();
+    }
+
     @Test
     public void step1Test() throws IOException {
-        DAY day = daySupplier.get();
-        STEP1 result = day.step1();
+        STEP1 result = day.resultStep1();
         Assertions.assertEquals(expected1, result);
     }
 
 
     @Test
     public void step2Test() throws IOException {
-        DAY day = daySupplier.get();
-        STEP2 result = day.step2();
+        STEP2 result = day.resultStep2();
         Assertions.assertEquals(expected2, result);
     }
+
+
 
 }
